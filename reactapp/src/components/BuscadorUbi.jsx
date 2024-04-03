f27b37f1d3e011009975a44cfef19024704fb978
juanant <carrilloherrerajuanantonio@gmail.com>
2024-04-04 00:24:24 +0200

    Inicio con API de Google Maps

---
+++ reactapp/src/components/BuscadorUbi.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
+export const BuscadorUbis = () => {
+    const [address, setAddress] = useState('');
+    const [coordenadas, setCoordenadas] = useState({})
+    const navigate = useNavigate();
+
+    const handleSelect = address => {
+        setAddress(address);
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => {
+                console.log('Success', latLng)
+                setCoordenadas(latLng)
+            })
+            .catch(error => console.error('Error', error));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('hooooooooooooola')
+        console.log(coordenadas)
+        navigate('/vehicle', {replace:true})
+
+    }
+
+    return (
+        <PlacesAutocomplete
+            value={address}
+            onChange={setAddress}
+            onSelect={handleSelect}
+        >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <form onSubmit={handleSubmit}>
+                    <input {...getInputProps({ placeholder: 'Buscar ubicaciones ...' })} />
+                    <div>
+                        {loading && <div>Cargando...</div>}
+                        {suggestions.map(suggestion => {
+                            return (
+                                <div key={suggestion.index} {...getSuggestionItemProps(suggestion)}>
+                                    <span>{suggestion.description}</span>
+                                    {/* {console.log(suggestion)} */}
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <input type="submit" value='Buscar' />
+                </form>
+            )}
+        </PlacesAutocomplete>
+    );
+}
--- reactapp/src/pages/Home.jsx
+++ reactapp/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Register } from './Register'
+import { BuscadorUbis } from '../components/BuscadorUbi'
 
 export const Home = () => {
   return (
     <>
         <h1>ConectaCar</h1>
+        <BuscadorUbis/>
+        <hr />
         <Link to='/register'>Registrate aquí</Link>
         <br />
         <Link to='/login'>Iniciar sesión</Link>

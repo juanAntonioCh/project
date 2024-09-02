45caafb18dc14219da7be8c3f16409b49cefd181
juanant <carrilloherrerajuanantonio@gmail.com>
2024-09-02 13:42:45 +0200

    google maps api key

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -24,7 +24,8 @@ import { NotFound } from './pages/NotFound';
 
 
 function App() {
-  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  //const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const apiKey = 'AIzaSyC_G0xCXyALB3IgkE5D4RpWWAxRIg9xCuQ'
   
   return (
     <LoadScript
